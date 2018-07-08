9712e7a5cd99ceefd7ff4cbdf9cfb57f0aae8778
sraban75 <nadimcst75@gmail.com>
2018-07-08 16:52:27 +0600

    Test

--- WinFormApp/WinFormApp/dwip.Designer.cs
+++ WinFormApp/WinFormApp/dwip.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormApp
 {
-    partial class FarabiForm
+    partial class dwip
     {
         /// <summary>
         /// Required designer variable.
@@ -29,43 +29,42 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(121, 64);
+            this.button1.Location = new System.Drawing.Point(66, 77);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Show";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // textBox1
             // 
-            this.button2.Location = new System.Drawing.Point(121, 111);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.textBox1.Location = new System.Drawing.Point(86, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
             // 
-            // FarabiForm
+            // dwip
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(288, 262);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Name = "FarabiForm";
-            this.Text = "FarabiForm";
+            this.Name = "dwip";
+            this.Text = "dwip";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- WinFormApp/WinFormApp/tanvir.Designer.cs
+++ WinFormApp/WinFormApp/tanvir.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormApp
 {
-    partial class FarabiForm
+    partial class Tanvir
     {
         /// <summary>
         /// Required designer variable.
@@ -29,36 +29,25 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(121, 64);
+            this.button1.Location = new System.Drawing.Point(159, 86);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Tanvir";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(121, 111);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // FarabiForm
+            // tanvir
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(448, 261);
             this.Controls.Add(this.button1);
-            this.Name = "FarabiForm";
-            this.Text = "FarabiForm";
+            this.Name = "tanvir";
+            this.Text = "tanvir";
             this.ResumeLayout(false);
 
         }
@@ -66,6 +55,5 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
