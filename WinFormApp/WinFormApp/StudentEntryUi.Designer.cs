b1276290ae48a19d3ca3f260805b9f467edc872a
UtshaAhmed <utshostm@gmail.com>
2018-07-08 17:02:41 +0600

    Updated by Utsha

--- WinFormApp/WinFormApp/StudentEntryUi.Designer.cs
+++ WinFormApp/WinFormApp/StudentEntryUi.Designer.cs
@@ -32,6 +32,8 @@
             this.nameTextBox = new System.Windows.Forms.TextBox();
             this.showButton = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // AddButton
@@ -74,11 +76,29 @@
             this.comboBox1.Size = new System.Drawing.Size(216, 21);
             this.comboBox1.TabIndex = 3;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(498, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // StudentEntryUi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1125, 664);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.showButton);
             this.Controls.Add(this.nameTextBox);
@@ -96,6 +116,8 @@
         private System.Windows.Forms.TextBox nameTextBox;
         private System.Windows.Forms.Button showButton;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
 
     }
 }
